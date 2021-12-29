49e563f9fbe4ecaa0913a962eb9b9b813b189f78
gotteweiss <96369498+gotteweiss@users.noreply.github.com>
2021-12-29 16:14:58 +0300

    Update arabify.js

--- arabify.js
+++ arabify.js
@@ -186,7 +186,7 @@ function phoneticToArabic(phoneticText){
 	}
 	if ((i==0 || !isWord(phoneticText[i-1][0]))&&(   isVowel(phoneticText[i][0])
 						      &&(    (phoneticText[i][0].toLowerCase() != 'а') 
-							  || (phoneticText[i][0].toLowerCase() != 'о'))){
+							  || (phoneticText[i][0].toLowerCase() != 'о')))){
 	    result += '\u0627'; // Аліф, калі галосная на пачатку слова
 	}
 	if (p in p2a) {
